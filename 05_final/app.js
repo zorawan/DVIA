44d6b3091779169a92260394950c50c6e714db4a
EC2 Default User <ec2-user@ip-172-31-25-25.ec2.internal>
2021-12-01 03:31:28 +0000

    final 5

--- 05_final/app.js
+++
@@ -1,2 +0,0 @@
-
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={0c87058cc940a123537a28bcde23b17b}
---
+++ 05_final/image.js
@@ -0,0 +1,2 @@
+
+api.openweathermap.org/data/2.5/weather?q={city name}&appid={0c87058cc940a123537a28bcde23b17b}
